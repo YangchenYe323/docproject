34bf6d445bd7f14faef573e14d53326e704e47d5
unknown <yye02@email.wm.edu>
2020-12-10 16:32:05 +0800

    initial commit

---
+++ src/main/java/yangchen/experiment/docproject/DocprojectApplication.java
@@ -0,0 +1,13 @@
+package yangchen.experiment.docproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DocprojectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DocprojectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/yangchen/experiment/docproject/DocprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package yangchen.experiment.docproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DocprojectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
