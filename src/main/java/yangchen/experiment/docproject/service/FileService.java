eb722b4686a5fbfdadeff09920d9351e11e30991
YangchenYe323 <yye02@email.wm.edu>
2020-12-14 08:48:57 +0800

    output file name improvement

--- src/main/java/yangchen/experiment/docproject/service/FileService.java
+++ src/main/java/yangchen/experiment/docproject/service/FileService.java
@@ -1,6 +1,10 @@
 package yangchen.experiment.docproject.service;
 
 import com.aspose.words.SaveFormat;
+import org.apache.pdfbox.io.RandomAccessBufferedFileInputStream;
+import org.apache.pdfbox.io.RandomAccessRead;
+
+import org.apache.pdfbox.pdfparser.PDFParser;
 import org.apache.pdfbox.pdmodel.PDDocument;
 import org.apache.poi.hwpf.HWPFDocument;
 import org.apache.poi.hwpf.converter.WordToHtmlConverter;
@@ -24,10 +28,10 @@ import java.nio.file.Paths;
 @Service
 public class FileService {
 
-
     public void processFile(MultipartFile file) throws Exception {
 
         String extension = getExtension(file.getOriginalFilename());
+        String fileName = getFileName(file.getOriginalFilename());
 
         System.out.println(Paths.get("").toAbsolutePath().toString());
 
@@ -39,15 +43,19 @@ public class FileService {
 
         if (extension.equals("pdf")) {
 
-            PDDocument document = PDDocument.load(f);
-            Writer output = new PrintWriter("src/output/out.html", "utf-8");
+            RandomAccessRead is = new RandomAccessBufferedFileInputStream(f);
+            PDFParser parser = new PDFParser(is);
+            parser.parse();
+
+            PDDocument document = parser.getPDDocument();
+            Writer output = new PrintWriter("src/output/" + fileName + ".html", "utf-8");
             new PDFDomTree().writeText(document, output);
             output.close();
         }
         else if(extension.equals("docx")){
 
             com.aspose.words.Document doc = new com.aspose.words.Document(new FileInputStream(f));
-            doc.save("src/output/docxout.html", SaveFormat.HTML);
+            doc.save("src/output/" + fileName + ".html", SaveFormat.HTML);
 
         }
         else if (extension.equals("doc")){
@@ -58,7 +66,7 @@ public class FileService {
             converter.processDocument(document);
 
             Document html = converter.getDocument();
-            OutputStream out = new FileOutputStream("src/output/docout.html");
+            OutputStream out = new FileOutputStream("src/output/" + fileName + ".html");
             DOMSource domSource = new DOMSource(html);
             StreamResult result = new StreamResult(out);
             TransformerFactory factory = TransformerFactory.newInstance();
@@ -71,6 +79,12 @@ public class FileService {
 
 
         }
+
+    }
+
+    private String getFileName(String originalFilename) {
+
+        return originalFilename.substring(0, originalFilename.lastIndexOf('.'));
 
     }
 
